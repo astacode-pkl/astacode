5cf10805556a117ea194cbdab0d466915def9ed2
tejar <tejarm04@gmail.com>
2025-02-04 09:59:12 +0700

    first

---
+++ components/Services.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="max-w-7xl">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div class="max-w-2xl text-center mx-auto">
+        <h2 class="block text-2xl font-bold text-blue-600 md:text-3xl">Our Services</h2>
+        <h3 class="block text-xl font-bold text-gray-800 md:text-2xl">We Provide the Best Quality</h3>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- Hero -->
+    <div class="relative overflow-hidden mt-20">
+      <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
+        <div class="max-w-2xl text-center mx-auto">
+          <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl">We Help Our Clients To Grow Their Business</h1>
+          <p class="mt-3 text-lg text-gray-800">Best it SOULTION Provider</p>
+        </div>
+        <div class="mt-10 relative max-w-5xl mx-auto">
+          <div
+            class="w-full object-cover h-96 sm:h-[480px] bg-[url('https://images.unsplash.com/photo-1606868306217-dbf5046868d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80')] bg-no-repeat bg-center bg-cover rounded-xl">
+          </div>
+          <!-- <div class="absolute inset-0 size-full">
+            <div class="flex flex-col justify-center items-center size-full">
+              <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                href="#">
+                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round">
+                  <polygon points="5 3 19 12 5 21 5 3" />
+                </svg>
+                Play the overview
+              </a>
+            </div>
+          </div> -->
+
+          <div
+            class="absolute bottom-12 -start-20 -z-[1] size-48 bg-gradient-to-b from-blue-500 to-white p-px rounded-lg">
+            <div class="bg-white size-48 rounded-lg"></div>
+          </div>
+
+          <div
+            class="absolute -top-12 -end-20 -z-[1] size-48 bg-gradient-to-t from-blue-600 to-blue-800 p-px rounded-full">
+            <div class="bg-white size-48 rounded-full"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Hero -->
+
+    <!-- services -->
+     
+     <!-- end services -->
+  </div>
+</template>
